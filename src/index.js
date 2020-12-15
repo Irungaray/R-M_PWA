ca726f0186937fa37fda8fdd089e3478284f6659
Irungaray <lionelirungaray@gmail.com>
2020-12-15 11:25:03 -0300

    Context

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import ThemeContext from './context/ThemeContext'
+
 import App from './App';
+
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeContext.Provider value="red">
+      <App />
+    </ThemeContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
