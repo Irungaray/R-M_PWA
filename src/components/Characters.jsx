b714de664b1396ae666103108c83aacb837e6374
Irungaray <lionelirungaray@gmail.com>
2020-12-16 07:28:17 -0300

    useReducer

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,34 +1,85 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from "react";
 
-import "./styles/Characters.css"
+import "./styles/Characters.css";
+
+const initialState = {
+  favorites: [],
+};
+
+const favoriteReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TO_FAVORITE":
+      return {
+        ...state,
+        favorites: [...state.favorites, action.payload],
+      };
+    default:
+      return state;
+  }
+};
 
 const Characters = () => {
   const [characters, setCharacters] = useState([]);
+  const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
 
   useEffect(() => {
-    fetch('https://rickandmortyapi.com/api/character/')
-      .then(response => response.json())
-      .then(data => setCharacters(data.results))
-      .catch(error => console.warn("Error in fetch:", error))
+    fetch("https://rickandmortyapi.com/api/character/")
+      .then((response) => response.json())
+      .then((data) => setCharacters(data.results))
+      .catch((error) => console.warn("Error in fetch:", error));
   }, []);
 
-  console.log(characters)
+  const handleClick = (favorite) => {
+    dispatch({
+      type: "ADD_TO_FAVORITE",
+      payload: favorite,
+    });
+  };
 
   return (
-    <div className="Characters">
-        {characters.map(character => (
-            <div className="Character" key={character.id}>
-                <h2> {character.name} </h2>
+    <>
+      <div className="Favorites">
+        <h2>Favorites:</h2>
+          {favorites.favorites.map((favorite) => (
+            <li
+              key={favorite.id}
+              className="FavList"
+            >
+              <button>
+                <img
+                    src="https://uxwing.com/wp-content/themes/uxwing/download/01-user_interface/red-x.png"
+                    alt=""
+                    className="Icon delete"
+                  />
+              </button>
+              {favorite.name}
+            </li>
+          ))}
+      </div>
 
-                <img src={character.image} alt="Character"/>
+      <div className="Characters">
+        {characters.map((character) => (
+          <div className="Character" key={character.id}>
+            <h2> {character.name} </h2>
 
-                <h4> Type: {character.species} </h4>
-                <h4> Gender: {character.gender} </h4>
-                <h4> Origin: {character.origin.name} </h4>
-            </div>
+            <img src={character.image} alt="Character" />
+
+            <h4> Type: {character.species} </h4>
+            <h4> Gender: {character.gender} </h4>
+            <h4> Origin: {character.origin.name} </h4>
+
+            <button type="button" onClick={() => handleClick(character)}>
+              <img
+                className="Icon"
+                src="https://icons.iconarchive.com/icons/hopstarter/soft-scraps/256/Button-Favorite-icon.png"
+                alt=""
+              />
+            </button>
+          </div>
         ))}
-    </div>
+      </div>
+    </>
   );
-}
+};
 
 export default Characters;
